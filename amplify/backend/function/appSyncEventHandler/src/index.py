0934433fa6a776e9a1fc92614467506587404d23
Michael Brewer <michael.brewer@gyft.com>
2021-11-18 09:33:23 -0800

    chore: add some powertools

--- amplify/backend/function/appSyncEventHandler/src/index.py
+++ amplify/backend/function/appSyncEventHandler/src/index.py
@@ -1,6 +1,14 @@
 import json
 
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.logging import correlation_paths
 
+logger = Logger()
+tracer = Tracer()
+
+
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.APPSYNC_RESOLVER)
+@tracer.capture_lambda_handler
 def handler(event, context):
     print("received event:")
     print(event)
