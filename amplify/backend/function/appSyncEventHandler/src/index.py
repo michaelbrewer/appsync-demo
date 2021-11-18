9ac73f971acd0e6600aacd7f4ece34d3054cdf84
Michael Brewer <michael.brewer@gyft.com>
2021-11-18 09:53:30 -0800

    chore: add feild resolvers

--- amplify/backend/function/appSyncEventHandler/src/index.py
+++ amplify/backend/function/appSyncEventHandler/src/index.py
@@ -1,16 +1,55 @@
 import json
 
 from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.event_handler import AppSyncResolver
 from aws_lambda_powertools.logging import correlation_paths
+from aws_lambda_powertools.utilities.data_classes.appsync import scalar_types_utils
 
 logger = Logger()
 tracer = Tracer()
+app = AppSyncResolver()
+
+LOCATIONS = [
+    {
+        "locationID": "foo",
+        "enabled": True,
+        "name": "Brew house",
+        "description": "String",
+        "creationTime": scalar_types_utils.aws_datetime(),  # type AWSDateTime
+    },
+    {
+        "locationID": scalar_types_utils.make_id(),
+        "name": "Another one",
+        "enabled": False,
+        "description": "String",
+        "done": True,
+        "creationTime": scalar_types_utils.aws_datetime(),
+    },
+]
+
+
+@app.resolver(type_name="Query", field_name="getLocation")
+def get_location(location_id: str = ""):
+    logger.info(f"Fetching location by id: {location_id}")
+    for location in LOCATIONS:
+        if location["locationID"] == id:
+            return location
+
+    raise ValueError("Not found")
+
+
+@app.resolver(type_name="Query", field_name="listLocations")
+def list_locations():
+    return LOCATIONS
+
+
+@app.resolver(field_name="commonField")
+def common_field():
+    # Would match all fieldNames matching 'commonField'
+    return scalar_types_utils.make_id()
 
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.APPSYNC_RESOLVER)
 @tracer.capture_lambda_handler
 def handler(event, context):
-    print("received event:")
-    print(event)
-
-    return [{"locationID": "1", "enabled": True, "name": "Brew House"}]
+    return app.resolve(event, context)
