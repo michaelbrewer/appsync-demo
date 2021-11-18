ed1b4423da52d0650f935e2480b1883368ed0074
Michael Brewer <michael.brewer@gyft.com>
2021-11-18 01:33:57 -0800

    chore: blank project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "demo": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
